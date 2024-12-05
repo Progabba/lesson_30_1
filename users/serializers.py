bc1456173b753778551bec6a3f36acfe724651ee
Albert <autelec@yandex.ru>
2024-12-06 00:03:58 +0400

    finish 31

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,26 @@
 from rest_framework import serializers
-from .models import Payment
+from .models import Payment, CustomUser
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = '__all__'
+
+
+# class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+#     @classmethod
+#     def get_token(cls, user):
+#         token = super().get_token(user)
+#
+#         # Добавление пользовательских полей в токен
+#         token['username'] = user.username
+#         token['email'] = user.email
+#
+#         return token
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = "__all__"
+
