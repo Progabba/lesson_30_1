6b5fc24a93e7623b3ad3d9e5b17b0521b3519b07
Albert <autelec@yandex.ru>
2024-12-22 17:31:15 +0400

    finish 33

--- lms/admin.py
+++ lms/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+from celery import Celery
+from celery.schedules import crontab
 
-# Register your models here.
+app = Celery('lms')
+
+app.conf.beat_schedule = {
+    'block-inactive-users-every-day': {
+        'task': 'lms.tasks.block_inactive_users',
+        'schedule': crontab(hour=0, minute=0),  # Запуск каждый день в полночь
+    },
+}
